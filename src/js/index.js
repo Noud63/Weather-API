d52850c2ed6cc880ba06e7ac89b0c33307baf9ad
Noud van Dun <38325801+Noud63@users.noreply.github.com>
2020-04-22 18:21:53 +0200

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 import {domStrings} from './views/domstrings';
 import Weather from './models/Search';
-import * as time from './models/Time';
+import * as time from './models/Time'; 
 import * as searchView from './views/searchView';
+
 
 // Date today:  weekday/ month/ day
 const now = new Date();
@@ -9,12 +10,14 @@ const options = { month: "long" , weekday: "long", day: "numeric"};
 const newTime = now.toLocaleDateString("en-EN", options);
 domStrings.day.textContent = newTime;
 
+
 // Date tomorrow: weekday/ month/ day
 const now2 = new Date();
-now2.setDate(now2.getDate() + 1)
+var n = now2.setDate(now2.getDate() + 1)
 const options2 = { month: "long" , weekday: "long", day: "numeric"};
-const newTime2 = now2.toLocaleDateString("en-EN", options2);
+var newTime2 = now2.toLocaleDateString("en-EN", options2);
 domStrings.day2.textContent = newTime2;
+
 
 //Search button 
 domStrings.button.addEventListener('click', function(e){
@@ -27,45 +30,55 @@ domStrings.button.addEventListener('click', function(e){
     searchView.clearInput();
     }
  });
-
- 
-// Control and process data
+let loc;
+let coord;
+ // Control and process data
 const controlData = async (input = 'Amsterdam') => {
-  
     try{
       
-      // Weather instance
       if(domStrings.input.value === ""){
         domStrings.city.textContent = 'Amsterdam'
+        domStrings.city2.textContent = 'Amsterdam'
       }
+      // Weather class instance
         Weather.newWeather = new Weather(input);
-        await Weather.newWeather.getData();
+                await Weather.newWeather.getData();
       
-      const temp =  Weather.newWeather.data.data.list[0].main.temp;
-                            domStrings.temperature.textContent =  temp.toFixed(1) + ' \xB0' + "C ";
-      const temp2 =  Weather.newWeather.data.data.list[1].main.temp;
-                            domStrings.temperature2.textContent =  temp2.toFixed(1) + ' \xB0' + "C ";
-  
-      const forecast =  Weather.newWeather.data.data.list[0].weather[0].description;
+
+      console.log(Weather.newWeather)  
+
+      let list = Weather.newWeather.data.data.list
+        
+      const temp = list[0].main.temp;      // temp_max optional
+                            domStrings.temperature.textContent =  Math.round(temp) + ' \xB0' + "C ";
+      const temp2 = list[8].main.temp;     // temp_max optional
+                            domStrings.temperature2.textContent =  Math.round(temp2) + ' \xB0' + "C ";
+                
+      const forecast = list[0].weather[0].description;
                             domStrings.foreCast.textContent = forecast.toLowerCase();
-      const forecast2 =  Weather.newWeather.data.data.list[1].weather[0].description;
+      const forecast2 = list[8].weather[0].description;
                             domStrings.foreCast2.textContent = forecast2.toLowerCase();
   
-      const icon =  Weather.newWeather.data.data.list[0].weather[0].icon;
+      const icon = list[0].weather[0].icon;
                             domStrings.weatherIcon.insertAdjacentHTML('afterbegin', `<img src="http://openweathermap.org/img/w/${icon}.png">`); 
-      const icon2 =  Weather.newWeather.data.data.list[1].weather[0].icon
+      const icon2 = list[8].weather[0].icon
                             domStrings.weatherIcon2.insertAdjacentHTML('afterbegin', `<img src="http://openweathermap.org/img/w/${icon2}.png">`);
   
-      let sRise = time.convertTime( Weather.newWeather.data.data.city.sunrise);
-      let sSet = time.convertTime( Weather.newWeather.data.data.city.sunset);
+      let sRise = time.convertTime(Weather.newWeather.data.data.city.sunrise);
+      let sSet = time.convertTime(Weather.newWeather.data.data.city.sunset);
                             domStrings.sunRise.textContent = `Sunrise today at ${sRise}`;
                             domStrings.sunSet.textContent = `Sunset today at ${sSet}`;
+                            
+                            /*
+                            let setTime = utcTime(Weather.newWeather.data.data.city.timezone, Weather.newWeather.data.data.city.name)
+                            console.log(setTime)  
+                            */            
     
     }catch(error){
                             domStrings.errorMessage.textContent = 'Oooops something went wrong!';
                 }
+                      
 };
   
 controlData();
-
-
+ 
